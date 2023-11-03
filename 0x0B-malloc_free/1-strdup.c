edd7d2e1ba759cc4b038ec662fceb655209abae0
Abdelrahman Arafa Ibrahim <bedo161999@gmail.com>
2023-11-03 18:50:00 +0000

    a

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include "main.h"
 #include <stdio.h>
 
@@ -14,15 +15,15 @@ char *_strdup(char *str)
     int i = 0, size = 0;
     char *m;
 
-    if(*str == NULL)
+    if(str == NULL)
         return(NULL);
     
     while (str[size])
 		size++;
 
-    *m = malloc(size * sizeof(*str)+1);
+    m = malloc(size * sizeof(*str)+1);
 
-    if(*m == NULL)
+    if(m == NULL)
         return(NULL);
     else
     {
