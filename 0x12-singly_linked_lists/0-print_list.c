e2ea4e79332a019c574c584a5d90bc18b94504ef
Abdelrahman Arafa Ibrahim <bedo161999@gmail.com>
2023-11-22 18:54:36 +0000

    a

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,11 +2,12 @@
 #include "lists.h"
 
 /**
- * print_list - prints all the elements of a linked list
+ * print_list - prints all the elements of a list_t list
  * @h: pointer to the list_t list to print
  *
- * Return: the number of nodes printed
+ * Return: the number of nodes in h
  */
+
 size_t print_list(const list_t *h)
 {
 	size_t s = 0;
@@ -18,7 +19,7 @@ size_t print_list(const list_t *h)
 		else
 			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
-		s++;
+		s++
 	}
 
 	return (s);
