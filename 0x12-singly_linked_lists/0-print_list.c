507e553c73465024483e72dcdf1873fdb9bfb4ea
Abdelrahman Arafa Ibrahim <bedo161999@gmail.com>
2023-11-22 18:51:25 +0000

    a

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,14 +15,11 @@ size_t print_list(const list_t *h)
 	while (h)
 	{
 		if (!h->str)
-		{
 			printf("[0] (nil)\n");
-		}
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		s++
-
-			h = h->next;
+		h = h->next;
 	}
 
 	return (s);
