4f7c50b67ad2151ee3fda9c8f01aa6ad563461fc
TaidaSa <abdelrahmanarafa124@gmail.com>
2024-01-27 11:33:13 +0200

    test

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - the entry point
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+
+int main(void)
+{
+    write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19",59);
+    return(0);
+}
