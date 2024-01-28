1f27f1183be552bad83445e6fbca17cdc6290e08
TaidaSa <abdelrahmanarafa124@gmail.com>
2024-01-28 16:40:32 +0200

    natural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - the entry point
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+
+int main(void)
+{
+    int i, j;
+
+    for (i = 0; i < 1024; i++)
+    {
+        if (i % 3 == 0 || i % 5 == 0)
+        {
+            j += i;
+        }
+    }
+    printf("%d\n", j);
+}
