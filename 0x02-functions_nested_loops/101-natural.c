444e5314c012f164be623b2459f34771578de97b
TaidaSa <abdelrahmanarafa124@gmail.com>
2024-01-28 16:48:13 +0200

    natural

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,14 +7,15 @@
 
 int main(void)
 {
-    int i, j;
+	int i, j;
 
-    for (i = 0; i < 1024; i++)
-    {
-        if (i % 3 == 0 || i % 5 == 0)
-        {
-            j += i;
-        }
-    }
-    printf("%d\n", j);
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			j += i;
+		}
+	}
+	printf("%d\n", j);
+	return(0);
 }
