06546de6c2f87ed85feff8b99d10b3d796e0f9d8
TaidaSa <abdelrahmanarafa124@gmail.com>
2024-01-28 17:26:24 +0200

    fip

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,16 +7,17 @@
 
 int main(void)
 {
-    long long int i, n1 = 1, n2 = 2, n3;
+	long long int i, n1 = 1, n2 = 2, n3;
 
-    printf("%lld, %lld, ", n1, n2);
+	printf("%lld, %lld, ", n1, n2);
 
-    for (i = 2; i < 50; ++i)
-    {
-        n3 = n1 + n2;
-        printf("%lld, ", n3);
-        n1 = n2;
-        n2 = n3;
-    }
-    printf("\n");
+	for (i = 2; i < 50; ++i)
+	{
+		n3 = n1 + n2;
+		printf("%lld, ", n3);
+		n1 = n2;
+		n2 = n3;
+	}
+	printf("\n");
+	return(0);
 }
