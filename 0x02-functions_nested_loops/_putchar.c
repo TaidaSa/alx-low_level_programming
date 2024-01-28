dcaa6d42f79eef19b70b7734e2ce3f22b19d8562
TaidaSa <abdelrahmanarafa124@gmail.com>
2024-01-28 10:58:18 +0200

    headers

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: the character to print
+ * Return: On success 1.
+ */
+
+void _putchar(char c)
+{
+    return(write(1, &c, 1));
+}
